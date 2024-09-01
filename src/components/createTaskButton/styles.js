82d49602102d054dd215f4c24707553dce4ad8a1
Joao Pedro PC <jpdrcbrl@gmail.com>
2024-08-31 21:07:30 -0300

    Melhorando botão addTask no mobile

--- src/components/createTaskButton/styles.js
+++ src/components/createTaskButton/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
-    position: absolute;
+    position: fixed;
     bottom: 30px;
     right: 30px;
     background-color: ${props => props.theme.color.transparent};
@@ -9,5 +9,10 @@ export const Button = styled.button`
     & svg{
         width: 50px;
         height: 50px;
+    }
+
+    @media (max-width: 767px) {
+        bottom: 20px;
+        right: 20px;
     }
 `
